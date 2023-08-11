0200f268d3e6d932949edc1fd99160be512516ed
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-08-11 14:29:16 +0100

    `Creates the first pass on the Player class.

---
+++ Joggle/Player.swift
@@ -0,0 +1,30 @@
+//
+//  Player.swift
+//  Joggle
+//
+//  Created by robin tetley on 11/08/2023.
+//
+
+import SwiftUI
+
+class Player: ObservableObject {
+    var usedWords = [String]()
+    var color: Color
+    
+    //positions of the letters on the board they've tapped so far. In this way we can check its adjacent neighbours.
+    @Published var selectedTiles = [Int]()
+    
+    init(color: Color) {
+        self.color = color
+    }
+    //resets that players game
+    func reset() {
+        selectedTiles.removeAll()
+        usedWords.removeAll()
+    }
+    
+    func trySelecting(_ index: Int, in game: Game) -> String? {
+        selectedTiles.append(index)
+        return nil
+    }
+}
