257a21f26e76ec5d66de2a2d4e787e49bef78e19
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-08-21 13:24:05 +0200

    Adds ResultsView and PlayerResults so we can show the users who won

---
+++ Joggle/PlayerResults.swift
@@ -0,0 +1,42 @@
+//
+//  PlayerResults.swift
+//  Joggle
+//
+//  Created by robin tetley on 21/08/2023.
+//
+
+import SwiftUI
+
+struct PlayerResults: View {
+    
+    var number: Int
+    var score: Int
+    //all the words they found first
+    var words: [String]
+    
+    
+    
+    var body: some View {
+        Section {
+            if words.isEmpty {
+                Text("None")
+            } else {
+                //since in boggle you can't have duplicate words using self for id is fine here.
+                ForEach(words, id: \.self) { word in
+                    Text("\(word) (\(word.score))")
+                }
+            }
+        } header: {
+            Text("Player \(number) score")
+        } footer: {
+            //he special “inflect” Markdown syntax tells SwiftUI we want the string to say “0 points”, “1 point”, “2 points”, etc – it pluralizes the string correctly for us.
+            Text("Total: ^[\(score) points](inflect: true)")
+        }
+    }
+}
+
+struct PlayerResults_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerResults(number: 1, score: 4, words: ["the", "cat", "sat", "mat"])
+    }
+}
---
+++ Joggle/ResultsView.swift
@@ -0,0 +1,58 @@
+//
+//  ResultsView.swift
+//  Joggle
+//
+//  Created by robin tetley on 21/08/2023.
+//
+
+import SwiftUI
+//view properties to store the words found by each player (excluding times when one player found a word after the other one), the scores each player received for those words, along with a string containing who won.
+
+
+struct ResultsView: View {
+    
+    var player1Words = [String]()
+    var player2Words = [String]()
+    var player1Score = 0
+    var player2Score = 0
+    var result: String
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                PlayerResults(number: 1, score: player1Score, words: player1Words)
+                PlayerResults(number: 2, score: player2Score, words: player2Words)
+            }
+            .navigationTitle(result)
+        }
+    }
+    
+    init(game: Game) {
+        //calculate player words and scores
+        for (word, player) in game.scores {
+            if player === game.player1 {
+                player1Score += word.score
+                player1Words.append(word)
+            }
+        }
+        //sort the resulting words
+        
+        player1Words.sort()
+        player2Words.sort()
+        
+        //calculate who won
+        if player1Score > player2Score {
+            result = "Player 1 wins!"
+        } else if player1Score < player2Score {
+            result = "Player 2 wins!"
+        } else {
+            result = "It's a draw!"
+        }
+    }
+}
+
+struct ResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultsView(game: Game())
+    }
+}
