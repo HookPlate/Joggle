cfb546f305d2e2de6f36e039af0a2e68460547c9
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-08-23 21:11:55 +0200

    .

--- Joggle/ContentView.swift
+++ Joggle/ContentView.swift
@@ -9,47 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @StateObject private var game = Game()
-    // place the timer view into a computed property then display it once normally and once with rotation
-    var timeRemainingText: Text {
-        if game.timeRemaining > 0 {
-            return
-            Text(Date.now.addingTimeInterval(game.timeRemaining), style: .timer)
-        } else {
-            return Text("0:00")
-        }
-    }
+ 
     
     var body: some View {
-        //places some space between our boards.
-        ZStack {
-            VStack(spacing: 60) {
-                LetterGridView(player: game.player2, game: game)
-                    .rotationEffect(.degrees(180))
-                LetterGridView(player: game.player1, game: game)
+        NavigationStack {
+            VStack {
+                NavigationLink {
+                    GameView()
+                } label: {
+                    Label("Show GameView", systemImage: "globe")
+                }
             }
-            .padding(10)
-            
-            HStack {
-                Spacer()
-                
-                timeRemainingText
-                    .rotationEffect(.degrees(180))
-                
-                Spacer()
-                
-                timeRemainingText
-                
-                Spacer()
-            }
-            .foregroundStyle(.white)
-            .font(.system(size: 36).monospacedDigit())
-            .frame(maxWidth: .infinity)
-            .frame(height: 40)
-            .background(.indigo)
-        }
-        .sheet(isPresented: $game.showingResults, onDismiss: game.reset) {
-            ResultsView(game: game)
         }
     }
 }
